e2e64ebf21336b6c8e0a84be96e859de324f0fcc
Chihab Hajji <chiheb-gcbdev@gulfofficial.com>
2023-11-12 22:46:23 +0100

    beep boop

--- src/modules/game/game.module.ts
+++ src/modules/game/game.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GameService } from './game.service';
 import { GameController } from './game.controller';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { Game } from './schema/game.entity';
 
 @Module({
-  imports: [GameModule], // Include your entity here
+  imports: [MikroOrmModule.forFeature([Game]), GameModule], // Include your entity here
   providers: [GameService],
   controllers: [GameController],
 })
